5495171c32973084e85e4dd7d104e9f17ec140c1
WojRud <wojciechrudol@gmail.com>
2025-01-23 21:47:55 +0100

    Initial Commit

---
+++ CryptoPage/src/main/java/com/CryptoPage/CryptoPage/CryptoPageApplication.java
@@ -0,0 +1,17 @@
+package com.CryptoPage.CryptoPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPageApplication.class, args);
+
+
+
+
+	}
+
+}
