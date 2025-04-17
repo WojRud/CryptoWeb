1987a5978606c46638dbc2815ea90fee967920e3
WojRud <wojciechrudol@gmail.com>
2025-04-17 23:04:05 +0200

    Unused imports deleted

--- CryptoPage/src/main/java/com/CryptoPage/CryptoPage/controller/MainController.java
+++ CryptoPage/src/main/java/com/CryptoPage/CryptoPage/controller/MainController.java
@@ -1,15 +1,8 @@
 package com.CryptoPage.CryptoPage.controller;
 
-import com.CryptoPage.CryptoPage.model.UserRepository;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Optional;
 
 @Controller
 public class MainController {
